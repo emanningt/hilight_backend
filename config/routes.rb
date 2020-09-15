17b94d885f5aeebe4f35da36a16af0d6677d340c
emanningt <emanningmusic@yahoo.com>
2020-09-15 12:43:46 -0700

    Update routes.rb
    I am adding routes that I think im going to end up using.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  get '/signup' => 'users#new'
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+
+  post '/users/:id' => 'users#show'
+
+  get '/logout' => 'sessions#destroy', method: :delete
+
   resources :notes
   resources :categories
   resources :users
