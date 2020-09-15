aa0ebcd7af96e7f4131221fcfc772fa61a677591
emanningt <emanningmusic@yahoo.com>
2020-09-15 12:23:51 -0700

    Update sessions_controller.rb
    I am setting up the sessions conrtoller so I can have a user run a session that eventually will allow them to store there notes under there own account.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,24 @@
 class SessionsController < ApplicationController
+
+    def new
+        user = User.new
+        render :login
+    end
+
+    def create 
+        user = User.find_by(username: params[:user][:username])
+        if user && user.authenticate(params[:user][:password])
+            session[:user_id] = user.id
+            redirect_to user_path(user)
+        else 
+            flash[:error] = "Well your not very good at loging in. Lets try again..."
+            redirect_to '/login'
+        end
+    end 
+
+    def destroy
+        session.delete(:user_id)
+        redirect_to '/'
+    end 
+    
 end
