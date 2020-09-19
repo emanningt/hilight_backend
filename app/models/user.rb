aa5a6130411ecbe7d9e358fa563e64b6e1ce668b
emanningt <emanningmusic@yahoo.com>
2020-09-19 13:16:44 -0700

    Updated modles
    updated all my modles to start testing in my console. I am focusing on making notes and categorys to work first and then ill come back and work on the user.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-    has_many :categorys 
-    has_many :notes 
-    has_secure_password 
+    # has_many :categorys 
+   # has_many :notes 
+    # has_secure_password 
 end
