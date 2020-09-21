0b0457c77d90b75ef743ef15512bb650120ad03a
emanningt <emanningmusic@yahoo.com>
2020-09-21 10:17:56 -0700

    Update application_controller.rb
    I wont be using a user for this api so i removed this unused code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,17 +2,5 @@ class ApplicationController < ActionController::API
 
     private
 
-  def logged_in?
-    !!session[:user_id]
-  end
-
-  def current_user
-    @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
-  end
-
-  def redirect_if_not_logged_in
-    redirect_to login_path if !logged_in?
-  end
-
   
 end
