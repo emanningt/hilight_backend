8bd3382143af34165a9d271a4e99a2938a8e5609
emanningt <emanningmusic@yahoo.com>
2020-09-22 09:48:24 -0700

    Update notes_controller.rb
    updated my notes controller with an index and create funtion. I also addes strong permas for notes.

--- app/controllers/api/v1/notes_controller.rb
+++ app/controllers/api/v1/notes_controller.rb
@@ -1,7 +1,24 @@
 class Api::V1::NotesController < ApplicationController
 
     def index
+        notes = Note.all
+        render json: notes
+    end 
 
+    def create
+        note = Note.new(note_params)
+        if note.save
+            render json: note, status:accepted
+        else 
+            render json: { errors: note.errors.full_messages }, status: :unprocessible_entity
+        end 
     end 
     
+
+    private
+
+    def note_params
+       params.permit(:note).permit(:title, :content, :category_id)
+    end 
+
 end
